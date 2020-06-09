a72270359ff09ca36d622fd4376b7a76837aea8b
Toan Pham <48141388+tpham61@users.noreply.github.com>
2020-06-09 14:07:40 -0700

    Trying to create and populate tables Still not working!!!

--- src/main/java/InnReservation.java
+++ src/main/java/InnReservation.java
@@ -29,7 +29,30 @@ public class InnReservation {
 	} catch (SQLException e) {
 	    System.err.println("SQLException: " + e.getMessage());
 	}
-    }
+	}
+	
+	private void initDb() throws SQLException {
+		try (Connection conn = DriverManager.getConnection(JDBC_URL,
+								   JDBC_USER,
+								   JDBC_PASSWORD)) {
+			try (Statement stmt = conn.createStatement()) {
+					stmt.execute("DROP TABLE IF EXISTS hp_goods");
+					boolean testCreateTable = stmt.execute("CREATE TABLE hp_goods (GId varchar(15) PRIMARY KEY, Food varchar(100), Flavor varchar(100), Price DECIMAL(5,1), AvailUntil DATE)");
+					System.out.format("Here: %b %n", testCreateTable);
+
+					stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('L1', 'Lemon', 'Cake', 20.0)");
+					stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('L2', 'Lemon', 'Twist', 3.50)");
+					stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('A3', 'Almond', 'Twist', 4.50)");
+					stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('A4', 'Almond', 'Cookie', 4.50)");
+					stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('L5', 'Lemon', 'Cookie', 1.50)");
+					stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('A6', 'Almond', 'Danish', 2.50)");
+					stmt.execute("DROP TABLE IF EXISTS lab7_reservations");
+					stmt.execute("CREATE TABLE lab7_reservations(Code INT(2), Room CHAR(5), CheckIn DATE, Checkout DATE, Rate FLOAT(5,2), LastName VARCHAR(15), FirstName VARCHAR(15), Adults INT(3), Kids INT(3),PRIMARY KEY (Code));");
+
+					stmt.execute("DROP TABLE IF EXISTS lab7_rooms");
+			}
+		}
+	}
     
     // Demo1 - Establish JDBC connection, execute DDL statement
     private void demo1() throws SQLException {
@@ -68,182 +91,167 @@ public class InnReservation {
     }
     
 
-    // Demo2 - Establish JDBC connection, execute SELECT query, read & print result
-    private void demo2() throws SQLException {
+    // // Demo2 - Establish JDBC connection, execute SELECT query, read & print result
+    // private void demo2() throws SQLException {
 
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    String sql = "SELECT * FROM hp_goods";
+	// // Step 1: Establish connection to RDBMS
+	// try (Connection conn = DriverManager.getConnection(JDBC_URL,
+	// 						   JDBC_USER,
+	// 						   JDBC_PASSWORD)) {
+	//     // Step 2: Construct SQL statement
+	//     String sql = "SELECT * FROM hp_goods";
 
-	    // Step 3: (omitted in this example) Start transaction
+	//     // Step 3: (omitted in this example) Start transaction
 
-	    // Step 4: Send SQL statement to DBMS
-	    try (Statement stmt = conn.createStatement();
-		 ResultSet rs = stmt.executeQuery(sql)) {
+	//     // Step 4: Send SQL statement to DBMS
+	//     try (Statement stmt = conn.createStatement();
+	// 	 ResultSet rs = stmt.executeQuery(sql)) {
 
-		// Step 5: Receive results
-		while (rs.next()) {
-		    String flavor = rs.getString("Flavor");
-		    String food = rs.getString("Food");
-		    float price = rs.getFloat("Price");
-		    System.out.format("%s %s ($%.2f) %n", flavor, food, price);
-		}
-	    }
+	// 	// Step 5: Receive results
+	// 	while (rs.next()) {
+	// 	    String flavor = rs.getString("Flavor");
+	// 	    String food = rs.getString("Food");
+	// 	    float price = rs.getFloat("Price");
+	// 	    System.out.format("%s %s ($%.2f) %n", flavor, food, price);
+	// 	}
+	//     }
 
-	    // Step 6: (omitted in this example) Commit or rollback transaction
-	}
-	// Step 7: Close connection (handled by try-with-resources syntax)
-    }
+	//     // Step 6: (omitted in this example) Commit or rollback transaction
+	// }
+	// // Step 7: Close connection (handled by try-with-resources syntax)
+    // }
 
 
-    // Demo3 - Establish JDBC connection, execute DML query (UPDATE)
-    // -------------------------------------------
-    // Never (ever) write database code like this!
-    // -------------------------------------------
-    private void demo3() throws SQLException {
+    // // Demo3 - Establish JDBC connection, execute DML query (UPDATE)
+    // // -------------------------------------------
+    // // Never (ever) write database code like this!
+    // // -------------------------------------------
+    // private void demo3() throws SQLException {
 
-        demo2();
+    //     demo2();
         
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.print("Enter a flavor: ");
-	    String flavor = scanner.nextLine();
-	    System.out.format("Until what date will %s be available (YYYY-MM-DD)? ", flavor);
-	    String availUntilDate = scanner.nextLine();
+	// // Step 1: Establish connection to RDBMS
+	// try (Connection conn = DriverManager.getConnection(JDBC_URL,
+	// 						   JDBC_USER,
+	// 						   JDBC_PASSWORD)) {
+	//     // Step 2: Construct SQL statement
+	//     Scanner scanner = new Scanner(System.in);
+	//     System.out.print("Enter a flavor: ");
+	//     String flavor = scanner.nextLine();
+	//     System.out.format("Until what date will %s be available (YYYY-MM-DD)? ", flavor);
+	//     String availUntilDate = scanner.nextLine();
 
-	    // -------------------------------------------
-	    // Never (ever) write database code like this!
-	    // -------------------------------------------
-	    String updateSql = "UPDATE hp_goods SET AvailUntil = '" + availUntilDate + "' " +
-		               "WHERE Flavor = '" + flavor + "'";
+	//     // -------------------------------------------
+	//     // Never (ever) write database code like this!
+	//     // -------------------------------------------
+	//     String updateSql = "UPDATE hp_goods SET AvailUntil = '" + availUntilDate + "' " +
+	// 	               "WHERE Flavor = '" + flavor + "'";
 
-	    // Step 3: (omitted in this example) Start transaction
+	//     // Step 3: (omitted in this example) Start transaction
 	    
-	    try (Statement stmt = conn.createStatement()) {
+	//     try (Statement stmt = conn.createStatement()) {
 		
-		// Step 4: Send SQL statement to DBMS
-		int rowCount = stmt.executeUpdate(updateSql);
+	// 	// Step 4: Send SQL statement to DBMS
+	// 	int rowCount = stmt.executeUpdate(updateSql);
 		
-		// Step 5: Handle results
-		System.out.format("Updated %d records for %s pastries%n", rowCount, flavor);		
-	    }
+	// 	// Step 5: Handle results
+	// 	System.out.format("Updated %d records for %s pastries%n", rowCount, flavor);		
+	//     }
 
-	    // Step 6: (omitted in this example) Commit or rollback transaction
+	//     // Step 6: (omitted in this example) Commit or rollback transaction
 	    
-	}
-	// Step 7: Close connection (handled implcitly by try-with-resources syntax)
+	// }
+	// // Step 7: Close connection (handled implcitly by try-with-resources syntax)
 
-        demo2();
+    //     demo2();
         
-    }
+    // }
 
 
-    // Demo4 - Establish JDBC connection, execute DML query (UPDATE) using PreparedStatement / transaction    
-    private void demo4() throws SQLException {
+    // // Demo4 - Establish JDBC connection, execute DML query (UPDATE) using PreparedStatement / transaction    
+    // private void demo4() throws SQLException {
 
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.print("Enter a flavor: ");
-	    String flavor = scanner.nextLine();
-	    System.out.format("Until what date will %s be available (YYYY-MM-DD)? ", flavor);
-	    LocalDate availDt = LocalDate.parse(scanner.nextLine());
+	// // Step 1: Establish connection to RDBMS
+	// try (Connection conn = DriverManager.getConnection(JDBC_URL,
+	// 						   JDBC_USER,
+	// 						   JDBC_PASSWORD)) {
+	//     // Step 2: Construct SQL statement
+	//     Scanner scanner = new Scanner(System.in);
+	//     System.out.print("Enter a flavor: ");
+	//     String flavor = scanner.nextLine();
+	//     System.out.format("Until what date will %s be available (YYYY-MM-DD)? ", flavor);
+	//     LocalDate availDt = LocalDate.parse(scanner.nextLine());
 	    
-	    String updateSql = "UPDATE hp_goods SET AvailUntil = ? WHERE Flavor = ?";
+	//     String updateSql = "UPDATE hp_goods SET AvailUntil = ? WHERE Flavor = ?";
 
-	    // Step 3: Start transaction
-	    conn.setAutoCommit(false);
+	//     // Step 3: Start transaction
+	//     conn.setAutoCommit(false);
 	    
-	    try (PreparedStatement pstmt = conn.prepareStatement(updateSql)) {
+	//     try (PreparedStatement pstmt = conn.prepareStatement(updateSql)) {
 		
-		// Step 4: Send SQL statement to DBMS
-		pstmt.setDate(1, java.sql.Date.valueOf(availDt));
-		pstmt.setString(2, flavor);
-		int rowCount = pstmt.executeUpdate();
+	// 	// Step 4: Send SQL statement to DBMS
+	// 	pstmt.setDate(1, java.sql.Date.valueOf(availDt));
+	// 	pstmt.setString(2, flavor);
+	// 	int rowCount = pstmt.executeUpdate();
 		
-		// Step 5: Handle results
-		System.out.format("Updated %d records for %s pastries%n", rowCount, flavor);
+	// 	// Step 5: Handle results
+	// 	System.out.format("Updated %d records for %s pastries%n", rowCount, flavor);
 
-		// Step 6: Commit or rollback transaction
-		conn.commit();
-	    } catch (SQLException e) {
-		conn.rollback();
-	    }
+	// 	// Step 6: Commit or rollback transaction
+	// 	conn.commit();
+	//     } catch (SQLException e) {
+	// 	conn.rollback();
+	//     }
 
-	}
-	// Step 7: Close connection (handled implcitly by try-with-resources syntax)
-    }
+	// }
+	// // Step 7: Close connection (handled implcitly by try-with-resources syntax)
+    // }
 
 
 
-    // Demo5 - Construct a query using PreparedStatement
-    private void demo5() throws SQLException {
+    // // Demo5 - Construct a query using PreparedStatement
+    // private void demo5() throws SQLException {
 
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.print("Find pastries with price <=: ");
-	    Double price = Double.valueOf(scanner.nextLine());
-	    System.out.print("Filter by flavor (or 'Any'): ");
-	    String flavor = scanner.nextLine();
+	// // Step 1: Establish connection to RDBMS
+	// try (Connection conn = DriverManager.getConnection(JDBC_URL,
+	// 						   JDBC_USER,
+	// 						   JDBC_PASSWORD)) {
+	//     Scanner scanner = new Scanner(System.in);
+	//     System.out.print("Find pastries with price <=: ");
+	//     Double price = Double.valueOf(scanner.nextLine());
+	//     System.out.print("Filter by flavor (or 'Any'): ");
+	//     String flavor = scanner.nextLine();
 
-	    List<Object> params = new ArrayList<Object>();
-	    params.add(price);
-	    StringBuilder sb = new StringBuilder("SELECT * FROM hp_goods WHERE price <= ?");
-	    if (!"any".equalsIgnoreCase(flavor)) {
-		sb.append(" AND Flavor = ?");
-		params.add(flavor);
-	    }
+	//     List<Object> params = new ArrayList<Object>();
+	//     params.add(price);
+	//     StringBuilder sb = new StringBuilder("SELECT * FROM hp_goods WHERE price <= ?");
+	//     if (!"any".equalsIgnoreCase(flavor)) {
+	// 	sb.append(" AND Flavor = ?");
+	// 	params.add(flavor);
+	//     }
 	    
-	    try (PreparedStatement pstmt = conn.prepareStatement(sb.toString())) {
-		int i = 1;
-		for (Object p : params) {
-		    pstmt.setObject(i++, p);
-		}
+	//     try (PreparedStatement pstmt = conn.prepareStatement(sb.toString())) {
+	// 	int i = 1;
+	// 	for (Object p : params) {
+	// 	    pstmt.setObject(i++, p);
+	// 	}
 
-		try (ResultSet rs = pstmt.executeQuery()) {
-		    System.out.println("Matching Pastries:");
-		    int matchCount = 0;
-		    while (rs.next()) {
-			System.out.format("%s %s ($%.2f) %n", rs.getString("Flavor"), rs.getString("Food"), rs.getDouble("price"));
-			matchCount++;
-		    }
-		    System.out.format("----------------------%nFound %d match%s %n", matchCount, matchCount == 1 ? "" : "es");
-		}
-	    }
+	// 	try (ResultSet rs = pstmt.executeQuery()) {
+	// 	    System.out.println("Matching Pastries:");
+	// 	    int matchCount = 0;
+	// 	    while (rs.next()) {
+	// 		System.out.format("%s %s ($%.2f) %n", rs.getString("Flavor"), rs.getString("Food"), rs.getDouble("price"));
+	// 		matchCount++;
+	// 	    }
+	// 	    System.out.format("----------------------%nFound %d match%s %n", matchCount, matchCount == 1 ? "" : "es");
+	// 	}
+	//     }
 
-	}
-    }
+	// }
+    // }
 
 
-    private void initDb() throws SQLException {
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    try (Statement stmt = conn.createStatement()) {
-                stmt.execute("DROP TABLE IF EXISTS hp_goods");
-                stmt.execute("CREATE TABLE hp_goods (GId varchar(15) PRIMARY KEY, Food varchar(100), Flavor varchar(100), Price DECIMAL(5,1), AvailUntil DATE)");
-                stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('L1', 'Lemon', 'Cake', 20.0)");
-                stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('L2', 'Lemon', 'Twist', 3.50)");
-                stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('A3', 'Almond', 'Twist', 4.50)");
-                stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('A4', 'Almond', 'Cookie', 4.50)");
-                stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('L5', 'Lemon', 'Cookie', 1.50)");
-                stmt.execute("INSERT INTO hp_goods (GId, Flavor, Food, Price) VALUES ('A6', 'Almond', 'Danish', 2.50)");
-	    }
-	}
-    }
+   
     
 
 }
